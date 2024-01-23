a41e3d2225823907a5d12c9b2070ca5d2b40c027
David Hinton <david.paul.hinton@proton.me>
2024-01-23 09:04:27 -0500

    initial comment - chapter 1

---
+++ src/main.rs
@@ -0,0 +1,99 @@
+// pulling in types and the namespaces from rltk
+use rltk::{GameState, Rltk, RGB};
+use specs::prelude::*;
+
+mod components;
+pub use components::*;
+
+mod player;
+use player::*;
+
+/*
+Classes in many other languages: 
+you can store a bunch of data inside them,
+ and you can also attach "methods" (functions) to them.
+*/
+struct State {
+    ecs: World
+}
+
+/*
+The state structure implements the trait GameState
+Traits are like interfaces
+*/
+impl GameState for State {
+    /*
+    &mut self:
+    &mut self means "this function requires access to the parent structure,
+    and may change it" (the mut is short for "mutable" - 
+    meaning it can change variables inside the structure - "state"). 
+     */
+
+    /*
+    ctx : &mut Rltk:
+    & means "pass a reference" - which is a pointer to an existing copy of the variable.
+    The variable isn't copied, you are working on the version that was passed in; 
+    if you make a change, you are changing the original.
+    
+    mut:
+    once again indicates that this is a "mutable" 
+    reference: you are allowed to make changes to the context.
+    
+    Rltk:
+    Finally Rltk is the type of the variable you are receiving.
+    In this case, it's a struct defined inside the RLTK library
+    that provides various things you can do to the screen.
+     */
+    fn tick(&mut self, ctx : &mut Rltk) {
+        ctx.cls();
+
+        player_input(self, ctx);
+
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+
+        for (pos, render) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
+    }
+}
+
+
+fn main() -> rltk::BError {
+    use rltk::RltkBuilder;
+    let context = RltkBuilder::simple80x50()
+        .with_title("Roguelike Tutorial")
+        .build()?;
+    let mut gs = State {
+        ecs: World::new()
+    };
+    
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
+    gs.ecs.register::<Player>();
+
+    gs.ecs
+        .create_entity()
+        .with(Position { x: 40, y: 25 })
+        .with(Renderable {
+            glyph: rltk::to_cp437('@'),
+            fg: RGB::named(rltk::YELLOW),
+            bg: RGB::named(rltk::BLACK),
+        })
+        .with(Player{})
+        .build();
+
+    for i in 0..10 {
+        gs.ecs
+        .create_entity()
+        .with(Position { x: i * 7, y: 20 })
+        .with(Renderable {
+            glyph: rltk::to_cp437('☺'),
+            fg: RGB::named(rltk::RED),
+            bg: RGB::named(rltk::BLACK),
+        })
+        .build();
+    }
+
+    rltk::main_loop(context, gs)
+}
